1ff399882fea0756d5225b16c64481ac9098d450
heddyk <hendersonkleber@gmail.com>
2021-09-16 15:15:15 -0300

    Configurado bundle inicial da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+/* Fonte */
+import 'fontsource-roboto'
+
+/* Routing */
+import { BrowserRouter as Router } from 'react-router-dom'
+import { RouterConfig } from './routing/RouterConfig'
+
+/* Css padrão */
 import './App.css';
 
-function App() {
+/* Provider responsavel por gerenciar os temas em toda a aplicação */
+import { ThemeProvider } from '@material-ui/core/styles';
+/* Temas customizados */
+import { dark, light } from './styles/themes';
+
+import { Button } from '@material-ui/core';
+
+import { styled } from '@material-ui/core/styles'
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const BackgroundDiv = styled('div')(({ theme }) => ({
+  width: '100%',
+  height: '100%',
+  background: theme.palette.background.default
+}))
+
+function App(): JSX.Element {
+  const [darkState, setDarkState] = useState<boolean>(false)
+
+  const handleThemeChange = () => setDarkState(!darkState)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={darkState ? dark : light}>
+        <CssBaseline />
+
+        <BackgroundDiv>
+          <h1>Hello</h1>
+
+          <Button onClick={handleThemeChange}>
+            Trocar tema
+          </Button>
+
+          <Router>
+            <RouterConfig />
+          </Router>
+        </BackgroundDiv>
+
+      </ThemeProvider>
+    </>
   );
 }
 
